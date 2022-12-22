8aec9a9b7d67f2a884aa2fca790883ded8d72e5c
Lucas Olimpio <lucasolimpio267@gmail.com>
2022-12-22 17:54:09 -0300

    adicionando metodo put

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -28,11 +28,32 @@ async function post(req, res) {
   product.save()
 
   res.send({
-    message: sucess,
+    message: "success",
   })
 }
 
+async function put(req, res) {
+  const {id} = req.params
+
+  const product = await ProductsModel.findOneAndUpdate({_id:id}, req.body, {new:true})
+
+  res.send({
+    message: "success",
+    product
+  })
+/*
+const product = await ProductsModel.findById( {_id:id} )
+
+  await product.updateOne(req.body)
+
+  res.send({
+    message: 'success',
+    product,
+  })
+*/
+}
 module.exports = {
   get,
   post,
+  put,
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,6 +10,6 @@ const ProductsController = require('../controllers/products')
 
 router.get('/products/:id?',ProductsController.get)
 router.post('/products', ProductsController.post)
-
+router.put('/products/:id', ProductsController.put)
 
 module.exports = router
